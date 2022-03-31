99cfe99e219de0c2cd99f17cdc9d9131ad0e69e4
dpsmnsk <dpsmnsk@gmail.com>
2022-03-31 21:47:59 +0500

    add function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const nodePath = require('path');
+const { config } = require('process');
 
 const gitlet = module.exports = {
     
@@ -21,5 +22,18 @@ const gitlet = module.exports = {
         };
 
         files.writeFilesFromTree(opts.bare ? gitletStructure : { ".gitlet": gitletStructure }, process.cwd());
-    }
+    },
+
+    add: function(path, _) {
+        files.assertInRepo();
+        config.assertNotBare();
+
+        const addedFiles = files.IsRecursive(path);
+
+        if (addedFiles.length === 0) {
+            throw new Error(files.pathFromRepoRoot(path) + " did not match any files");
+        } else {
+            addedFiles.forEach(function(p) { gitlet.update_index(p, { add: true }); });
+        }
+    },
 }
