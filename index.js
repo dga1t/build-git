5f08ebfdf0e2a577ccade9931dc9f491886e8e52
dpsmnsk <dpsmnsk@gmail.com>
2022-04-21 21:40:51 +0500

    clone func added

--- index.js
+++ index.js
@@ -318,5 +318,39 @@ const gitlet = module.exports = {
         return statuss.toString();
     },
 
-    
+    clone: function (remotePath, targetPath, opts) {
+        opts = opts || {};
+
+        if (remotePath === undefined || targetPath === undefined) {
+            throw new Error("you must specify remote path and target path");
+
+        } else if (!fs.existsSync(remotePath) || !util.onRemote(remotePath)(files.inRepo)) {
+            throw new Error("repository " + remotePath + " does not exist");
+
+        } else if (fs.existsSync(targetPath) && fs.readdirSync(targetPath).length > 0) {
+            throw new Error(targetPath + " already exists and is not empty");
+
+        } else {
+
+            remotePath = nodePath.resolve(process.cwd(), remotePath);
+
+            if (!fs.existsSync(targetPath)) fs.mkdirSync(targetPath);
+
+            util.onRemote(targetPath)(function () {
+
+                gitlet.init(opts);
+
+                gitlet.remote("add", "origin", nodePath.relative(process.cwd(), remotePath));
+
+                const remoteHeadHash = util.onRemote(remotePath)(refs.hash, "master");
+
+                if (remoteHeadHash !== undefined) {
+                    gitlet.fetch("origin", "master");
+                    merge.writeFastForwardMerge(undefined, remoteHeadHash);
+                }
+            });
+
+            return "Cloning into " + targetPath;
+        }
+    }
 }
