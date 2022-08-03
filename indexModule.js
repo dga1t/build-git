9060efa8e8380d2d276dcb9714388594090f4d8e
dpsmnsk <dpsmnsk@gmail.com>
2022-08-03 22:57:23 +0500

    index module done

--- indexModule.js
+++ indexModule.js
@@ -1,35 +1,113 @@
 const index = {
+  hasFile: function (path, stage) {
+    return index.read()[index.key(path, stage)] !== undefined;
+  },
 
-    hasFile: function(path, stage) {
-        return index.read()[index.key(path, stage)] !== undefined;
-    },
+  read: function () {
+    const indexFilePath = files.gitletPath("index");
 
-    read: function() {
-        const indexFilePath = files.gitletPath("index");
+    return util
+      .lines(fs.existsSync(indexFilePath) ? files.read(indexFilePath) : "\n")
+      .reduce(function (idx, blobStr) {
+        const blobData = blobStr.split(/ /);
+        idx[index.key(blobData[0], blobData[1])] = blobData[2];
+        return idx;
+      }, {});
+  },
 
-        return util.lines(fs.existsSync(indexFilePath) ? files.read(indexFilePath) : "\n")
-            .reduce(function(idx, blobStr) {
-                const blobData = blobStr.split(/ /);
-                idx[index.key(blobData[0], blobData[1])] = blobData[2];
-                return idx;
-            }, {});
-    },
+  key: function (path, stage) {
+    return path + "," + stage;
+  },
 
-    key: function(path, stage) {
-        return path + "," + stage;
-    },
+  keyPieces: function (key) {
+    const pieces = key.split(/,/);
+    return { path: pieces[0], stage: parseInt(pieces[1]) };
+  },
 
-    keyPieces: function(key) {
-        const pieces = key.split(/,/);
-        return { path: pieces[0], stage: parseInt(pieces[1]) };
-    },
+  toc: function () {
+    const idx = index.read();
+    return Object.keys(idx).reduce(function (obj, k) {
+      return util.setIn(obj, [k.split(",")[0], idx[k]]);
+    }, {});
+  },
 
-    toc: function() {
-        const idx = index.read();
-        return Object.keys(idx).reduce(function(obj, k) { return util.setIn(obj, [k.split(",")[0], idx[k]]); }, {});
-    },
+  isFileInConflict: function (path) {
+    return index.hasFile(path, 2);
+  },
 
-    isFileInConflict: function(path) {
-        return index.hasFile(path, 2);
-    },
-}
+  conflictedPaths: function () {
+    const idx = index.read();
+    return Object.keys(idx)
+      .filter(function (k) {
+        return index.keyPieces(k).stage === 2;
+      })
+      .map(function (k) {
+        return index.keyPieces(k).path;
+      });
+  },
+
+  writeNonConflict: function (path, content) {
+    index.writeRm(path);
+    index._writeStageEntry(path, 0, content);
+  },
+
+  writeConflict: function (path, receiverContent, giverContent, baseContent) {
+    if (baseContent !== undefined) {
+      index._writeStageEntry(path, 1, baseContent);
+    }
+
+    index._writeStageEntry(path, 2, receiverContent);
+    index._writeStageEntry(path, 3, giverContent);
+  },
+
+  writeRm: function (path) {
+    const idx = index.read();
+    [0, 1, 2, 3].forEach(function (stage) {
+      delete idx[index.key(path, stage)];
+    });
+    index.write(idx);
+  },
+
+  _writeStageEntry: function (path, stage, content) {
+    const idx = index.read();
+    idx[index.key(path, stage)] = objects.write(content);
+    index.write(idx);
+  },
+
+  write: function (index) {
+    const indexStr =
+      Object.keys(index)
+        .map(function (k) {
+          return k.split(",")[0] + " " + k.split(",")[1] + " " + index[k];
+        })
+        .join("\n") + "\n";
+    files.write(files.gitletPath("index"), indexStr);
+  },
+
+  workingCopyToc: function () {
+    return Object.keys(index.read())
+      .map(function (k) {
+        return k.split(",")[0];
+      })
+      .filter(function (p) {
+        return fs.existsSync(files.workingCopyPath(p));
+      })
+      .reduce(function (idx, p) {
+        idx[p] = util.hash(files.read(files.workingCopyPath(p)));
+        return idx;
+      }, {});
+  },
+
+  tocToIndex: function (toc) {
+    return Object.keys(toc).reduce(function (idx, p) {
+      return util.setIn(idx, [index.key(p, 0), toc[p]]);
+    }, {});
+  },
+
+  matchingFiles: function (pathSpec) {
+    const searchPath = files.pathFromRepoRoot(pathSpec);
+    return Object.keys(index.toc()).filter(function (p) {
+      return p.match("^" + searchPath.replace(/\\/g, "\\\\"));
+    });
+  },
+};
