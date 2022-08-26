9f835dab91d3f7ae90aafec471b0be605334a4db
Dima Pasumansky <dpsmnsk@gmail.com>
2022-08-26 22:46:50 +0500

    files module done

--- filesModule.js
+++ filesModule.js
@@ -50,4 +50,80 @@ const files = {
       }
     }
   },
+
+  read: function (path) {
+    if (fs.existsSync(path)) {
+      return fs.readFileSync(path, 'utf8');
+    }
+  },
+
+  gitletPath: function (path) {
+    function gitletDir(dir) {
+      if (fs.existsSync(dir)) {
+        const potentialConfigFile = nodePath.join(dir, 'config');
+        const potentialGitletPath = nodePath.join(dir, '.gitlet');
+        if (
+          fs.existsSync(potentialConfigFile) &&
+          fs.statSync(potentialConfigFile).isFile() &&
+          files.read(potentialConfigFile).match(/\[core\]/)
+        ) {
+          return dir;
+        } else if (fs.existsSync(potentialGitletPath)) {
+          return potentialGitletPath;
+        } else if (dir !== '/') {
+          return gitletDir(nodePath.join(dir, '..'));
+        }
+      }
+    }
+
+    const gDir = gitletDir(process.cwd());
+    if (gDir !== undefined) {
+      return nodePath.join(gDir, path || '');
+    }
+  },
+
+  workingCopyPath: function (path) {
+    return nodePath.join(nodePath.join(files.gitletPath(), '..'), path || '');
+  },
+
+  lsRecursive: function (path) {
+    if (!fs.existsSync(path)) {
+      return [];
+    } else if (fs.statSync(path).isFile()) {
+      return [path];
+    } else if (fs.statSync(path).isDirectory()) {
+      return fs.readdirSync(path).reduce(function (fileList, dirChild) {
+        return fileList.concat(
+          files.lsRecursive(nodePath.join(path, dirChild))
+        );
+      }, []);
+    }
+  },
+
+  nestFlatTree: function (obj) {
+    return Object.keys(obj).reduce(function (tree, wholePath) {
+      return util.setIn(
+        tree,
+        wholePath.split(nodePath.sep).concat(obj[wholePath])
+      );
+    }, {});
+  },
+
+  flattenNestedTree: function (tree, obj, prefix) {
+    if (obj === undefined) {
+      return files.flattenNestedTree(tree, {}, '');
+    }
+
+    Object.keys(tree).forEach(function (dir) {
+      const path = nodePath.join(prefix, dir);
+      
+      if (util.isString(tree[dir])) {
+        obj[path] = tree[dir];
+      } else {
+        files.flattenNestedTree(tree[dir], obj, path);
+      }
+    });
+
+    return obj;
+  },
 };
